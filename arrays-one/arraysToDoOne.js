79538fafd754284b885b4232aa8b50c4791e965a
melissaAcheng <melcheng12@gmail.com>
2022-05-05 10:56:19 -0400

    popFront function removes and returns the value at the beginning of the array without using any built-in-array methods except pop()

--- arrays-one/arraysToDoOne.js
+++ arrays-one/arraysToDoOne.js
@@ -11,4 +11,20 @@ function pushFront(arr, val) {
   return arr;
 }
 
-console.log(pushFront(["a", "b", "c", "d", "e"], "first"));
+console.log("PUSH FRONT =>", pushFront(["a", "b", "c", "d", "e"], "first"));
+
+// Pop Front
+// Given an array, remove and return the value at the beginning of the array. Do this without using any built-in array methods except pop().
+
+function popFront(arr) {
+  const firstValue = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    arr[i - 1] = arr[i];
+  }
+  arr.pop();
+  console.log("new array", arr);
+  return firstValue;
+}
+
+console.log("POP FRONT =>", popFront(["a", "b", "c", "d", "e"]));
