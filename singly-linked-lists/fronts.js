03db78d255dfc79fd164f207f7f0d385a280e63d
melissaAcheng <melcheng12@gmail.com>
2022-05-09 11:16:44 -0400

    functions max, min, average

--- singly-linked-lists/fronts.js
+++ singly-linked-lists/fronts.js
@@ -46,13 +46,13 @@ class SLL {
   }
   length() {
     let runner = this.head;
-    let sum = 0;
+    let length = 0;
 
     while (runner !== null) {
-      sum++;
+      length++;
       runner = runner.next;
     }
-    return sum;
+    return length;
   }
   display() {
     let runner = this.head;
@@ -64,14 +64,58 @@ class SLL {
     }
     return list;
   }
+  max() {
+    let runner = this.head;
+    let max = 0;
+
+    while (runner !== null) {
+      if (runner.data > max) {
+        max = runner.data;
+        runner = runner.next;
+      } else {
+        runner = runner.next;
+      }
+    }
+    return max;
+  }
+  min() {
+    let runner = this.head;
+    let min = runner.data;
+
+    while (runner !== null) {
+      if (runner.data < min) {
+        min = runner.data;
+        runner = runner.next;
+      } else {
+        runner = runner.next;
+      }
+    }
+    return min;
+  }
+  average() {
+    let runner = this.head;
+    let sum = 0;
+    let length = 0;
+
+    while (runner !== null) {
+      sum += runner.data;
+      length++;
+      runner = runner.next;
+    }
+    return sum / length;
+  }
 }
 
 let newList = new SLL();
-console.log(newList.addFront(1));
-console.log(newList.addFront(2));
 console.log(newList.addFront(3));
+console.log(newList.addFront(5));
+console.log(newList.addFront(1));
 console.log(newList.removeFront());
+console.log(newList.addFront(1));
 console.log(newList.front());
 console.log(newList.contains(1));
 console.log(newList.length());
 console.log(newList.display());
+console.log(newList.max());
+console.log(newList.min());
+console.log(newList.average());
