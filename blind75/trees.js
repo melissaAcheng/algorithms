720e985d3aa49690aedc34633b67175dce779916
melissaAcheng <melcheng12@gmail.com>
2022-11-01 14:17:13 -0400

    binary tree left view

--- blind75/trees.js
+++ blind75/trees.js
@@ -34,12 +34,45 @@ const findMax = function (root) {
   return result;
 };
 
-var root = new TreeNode(12);
+// var root = new TreeNode(12);
+// root.left = new TreeNode(7);
+// root.right = new TreeNode(1);
+// root.left.left = new TreeNode(9);
+// root.left.right = new TreeNode(2);
+// root.right.left = new TreeNode(10);
+// root.right.right = new TreeNode(5);
+
+// console.log(`Level largest values are: ${findMax(root)}`);
+
+// Given a binary tree, return an array containing nodes in its left view
+const leftView = (root) => {
+  const result = [];
+
+  const queue = [root];
+  while (queue.length > 0) {
+    let levelSize = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      if (i === levelSize - 1) result.push(node.value);
+      if (node.right !== null) queue.push(node.right);
+      if (node.left !== null) queue.push(node.left);
+    }
+  }
+
+  return result;
+}
+
+const root = new TreeNode(12);
 root.left = new TreeNode(7);
 root.right = new TreeNode(1);
 root.left.left = new TreeNode(9);
-root.left.right = new TreeNode(2);
 root.right.left = new TreeNode(10);
 root.right.right = new TreeNode(5);
-
-console.log(`Level largest values are: ${findMax(root)}`);
+root.left.left.left = new TreeNode(3);
+result = leftView(root);
+console.log(result);
+process.stdout.write('Tree right view: ');
+for (i = 0; i < result.length; i++) {
+  process.stdout.write(`${result[i]} `);
+}
