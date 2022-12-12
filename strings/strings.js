f4c789d4b5eada50fcf0a837414db9f5de49ced9
melissaAcheng <melcheng12@gmail.com>
2022-12-12 12:47:50 -0500

    solved productExceptSelf in O(n) time and space

--- strings/strings.js
+++ strings/strings.js
@@ -26,3 +26,37 @@ console.log(isAnagram("anagram", "nagaram"));
 // n = length of string s or t
 // Time: O(n)
 // Space: O(n)
+
+//238. Product of Array Except Self
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+function productExceptSelf(nums) {
+  const prefix = Array(nums.length).fill(0);
+    const postfix = Array(nums.length).fill(0);
+    const result = Array(nums.length).fill(0);
+
+    prefix[0] = 1;
+    for (let i = 1; i < nums.length; i++) {
+        prefix[i] = prefix[i-1] * nums[i-1];
+    }
+
+    postfix[nums.length - 1] = 1;
+    for (let i = nums.length - 2; i >= 0; i--) {
+        postfix[i] = postfix[i+1] * nums[i+1];
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = prefix[i] * postfix[i];
+    }   
+
+    return result;
+}
+
+console.log(productExceptSelf([1, 2, 3, 4]));
+// n = number of elements in array
+// Time = O(n)
+// Space = O(n)
